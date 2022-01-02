885cf71d8f1e8630f7a9fb044cc573250191587a
beel <abdallah98@gmail.com>
2022-01-02 23:05:45 +0200

    Implementing the Ellipse

--- app/src/main/java/com/example/projectmob/ui/ellipse/NavEllipseViewModel.java
+++ app/src/main/java/com/example/projectmob/ui/ellipse/NavEllipseViewModel.java
@@ -1,7 +1,14 @@
 package com.example.projectmob.ui.ellipse;
 
+import static java.lang.Float.parseFloat;
+
 import androidx.lifecycle.ViewModel;
 
 public class NavEllipseViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    public String calculate(String x,String y){
+        Double xd = Double.valueOf(x);
+        Double yd = Double.valueOf(y);
+        Double result = xd*yd*3.14;
+        return String.valueOf(result);
+    }
 }
--- app/src/main/java/com/example/projectmob/ui/ellipse/nav_ellipse.java
+++ app/src/main/java/com/example/projectmob/ui/ellipse/nav_ellipse.java
@@ -11,28 +11,48 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.projectmob.R;
+import com.example.projectmob.databinding.NavCircleFragmentBinding;
+import com.example.projectmob.databinding.NavEllipseFragmentBinding;
+import com.example.projectmob.ui.circle.NavCircleViewModel;
 
 public class nav_ellipse extends Fragment {
 
-    private NavEllipseViewModel mViewModel;
-
+    private NavEllipseViewModel navEllipseViewModel;
+    private NavEllipseFragmentBinding binding;
     public static nav_ellipse newInstance() {
         return new nav_ellipse();
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.nav_ellipse_fragment, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        navEllipseViewModel =
+                new ViewModelProvider(this).get(NavEllipseViewModel.class);
+
+        binding = NavEllipseFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        final Button button = binding.button;
+        final TextView a = binding.atxt;
+        final TextView b = binding.btxt;
+        final  TextView result = binding.result;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final  String astr=  a.getText().toString();
+                final  String bstr=  b.getText().toString();
+                result.setText(navEllipseViewModel.calculate(astr,bstr));
+            }
+        });
+        return root;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(NavEllipseViewModel.class);
-        // TODO: Use the ViewModel
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
