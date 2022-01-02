2d5192e4bcb8babe8a8e9e2b265ba4dd6656dc7a
beel <abdallah98@gmail.com>
2022-01-02 22:35:36 +0200

    Implementing the Circle

--- app/src/main/java/com/example/projectmob/ui/circle/NavCircleViewModel.java
+++ app/src/main/java/com/example/projectmob/ui/circle/NavCircleViewModel.java
@@ -7,17 +7,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class NavCircleViewModel extends ViewModel {
-
-    private MutableLiveData<String> mText;
-
-    public NavCircleViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is circle fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
     public String calculate(String x){
         return Double.toString((Math.pow(parseFloat(x), 2) * 3.14));
     }
