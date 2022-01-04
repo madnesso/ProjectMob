f2c3291d54c5cad5e7b88bfeeafb0e1441f41e06
beel <abdallah98@gmail.com>
2022-01-04 17:21:26 +0200

    Implementing Triangle

--- app/src/main/java/com/example/projectmob/ui/triangle/NavTriangleViewModel.java
+++ app/src/main/java/com/example/projectmob/ui/triangle/NavTriangleViewModel.java
@@ -3,5 +3,10 @@ package com.example.projectmob.ui.triangle;
 import androidx.lifecycle.ViewModel;
 
 public class NavTriangleViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    public String calculate(String x,String y){
+        Double xd = Double.valueOf(x);
+        Double yd = Double.valueOf(y);
+        Double result = xd*yd*0.5;
+        return String.valueOf(result);
+    }
 }
--- app/src/main/java/com/example/projectmob/ui/triangle/nav_triangle.java
+++ app/src/main/java/com/example/projectmob/ui/triangle/nav_triangle.java
@@ -11,28 +11,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.projectmob.R;
+import com.example.projectmob.databinding.NavRectangleBinding;
+import com.example.projectmob.databinding.NavTriangleFragmentBinding;
+import com.example.projectmob.ui.rectangle.NavRectangleViewModel;
 
 public class nav_triangle extends Fragment {
 
-    private NavTriangleViewModel mViewModel;
+    private NavTriangleViewModel navTriangleViewModel;
+    private NavTriangleFragmentBinding binding;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        navTriangleViewModel =
+                new ViewModelProvider(this).get(NavTriangleViewModel.class);
 
-    public static nav_triangle newInstance() {
-        return new nav_triangle();
+        binding = NavTriangleFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final Button button = binding.button;
+        final TextView a = binding.basetxt;
+        final TextView b = binding.heighttxt;
+        final TextView result = binding.result;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String astr = a.getText().toString();
+                final String bstr = b.getText().toString();
+                result.setText(navTriangleViewModel.calculate(astr, bstr));
+            }
+        });
+        return root;
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.nav_triangle_fragment, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(NavTriangleViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
